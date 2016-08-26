2899efdfd5fb53e480766f7e1e59212aa8854b38
albiebabie <albiebabie@gmail.com>
2016-08-26 11:02:03 +0100

    one review per user- partially done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ActiveRecord::Base
 
-  has_many :restaurants
+  has_many :restaurants, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
-
-   def self.from_omniauth(auth)
-     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-       user.email = auth.info.email
-       user.password = Devise.friendly_token[0,20]
-     end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 
 end
